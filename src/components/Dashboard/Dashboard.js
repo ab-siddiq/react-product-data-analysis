c58b7a76f25ea0699b5765c504e718151c25a162
siddiq <siddiq.aiub@gmail.com>
2022-04-04 13:01:31 +0600

    completed dashboard showed chart using rechart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+        .then(data=>setData(data))
+    }, [])
+    console.log(data)
     return (
-        <div>
-            /Dashboard
+        <div className='grid grid-cols-2 m-20'>
+            <div className="">
+                <LineChart width={500} height={400} data={data}>
+                    <Line dataKey="investment"></Line>
+                    <CartesianGrid></CartesianGrid>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <div className="">
+                <BarChart width={500} height={400} data={data}>
+                <Line dataKey="investment"></Line>
+                    <CartesianGrid></CartesianGrid>
+                    <XAxis dataKey="month" stroke="#8884d8"></XAxis>
+                    <YAxis></YAxis>
+                    <Bar dataKey="investment" fill="#8884d8" barSize={20}></Bar>
+                    <Tooltip></Tooltip>
+                </BarChart>
+            </div>
         </div>
     );
 };
